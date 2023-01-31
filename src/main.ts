491eea6bd12ed172bc56f0163003f834a5e5660b
Le Yang <le.yang@gmail.com>
2023-01-30 23:13:29 -0500

    404

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,14 @@ import { RootComponent } from './app/components/root/root.component';
 import { UserListComponent } from './app/components/user-list/user-list.component';
 import { reducers, metaReducers } from './app/state/reducers';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './app/components/not-found/not-found.component';
 
 
 bootstrapApplication(RootComponent, {
   providers: [
     provideRouter([
       { path: 'user-list', component: UserListComponent },
+      { path: 'not-found', component: NotFoundComponent },
       { path: '', pathMatch: 'full', redirectTo: 'user-list' },
       { path: '**', redirectTo: 'user-list' }
     ]),
